1ab8182403b9fc59a32de96bb17892a5ad77324c
christhopher.leao <christhopherleao@icloud.com>
2019-02-04 18:04:10 -0200

    Scaffolded Project

---
+++ src/components/SearchPage/SearchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchPage = () => (
+  <main id="SearchPage">
+    <h1>DROVER</h1>
+    <h3>Search</h3>
+    <p>Welcome!</p>
+  </main>
+);
+
+export default SearchPage;
